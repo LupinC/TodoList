e23fd78842f9e95260c8a973a53630fc100bec6b
Lupin(Yuan) Cai <lupin.cai@emory.edu>
2024-05-10 16:04:43 -0400

    feature: ..

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import "./styles.css"
 
 
-
+// this is a componenet
 export default function App(){
-  return <form className="New-item-form">
-
+  return <form className="new-item-form">
+    <div className="row">
+      <label htmlFor="item">New Item</label>
+      <input type="text" id="item"/>
+    </div>
   </form>
 }
